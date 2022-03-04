1a36c82983b6397eb4fc809f72f07254cbae56c5
willdrr <willxdd97@gmail.com>
2022-03-04 14:07:03 +0000

    generate product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_205339) do
+ActiveRecord::Schema.define(version: 2022_03_04_131317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_03_03_205339) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
