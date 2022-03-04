5f470195998e2744fd401ddd8e97297a980d9c11
willdrr <willxdd97@gmail.com>
2022-03-04 11:50:14 +0000

    restrict actions for unauthorized users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -5,11 +5,46 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test 'should show user' do
-    get api_v1_user_url(@user), as: :json
-    assert_response :success
+  test 'should create user' do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: 'test@etest.org', password: '123456'
+      } }, as: :json
+    end
+    assert_response :created
+  end
 
-    json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
+  test 'shoud not create user with taken email' do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as:
+	:json
+    end
+    assert_response :unprocessable_entity
+  end
+
+  # test related to jwt token user
+  test 'should update user' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, headers: {
+      Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
+    assert_response :success
+  end
+
+  test 'should forbid update user' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
+  end
+
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), headers: { Authorization: JsonWebToken
+	.encode(user_id: @user.id) }, as: :json
+    end
+    assert_response :no_content
+  end
+
+  test 'should forbid destroy user' do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 end
