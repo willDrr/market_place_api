84ad6e32967ba9e34d8b4d40c863cf3a7a63b030
willdrr <willxdd97@gmail.com>
2022-03-05 00:08:54 +0000

    add user relationship to product serializer

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -11,7 +11,8 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
     json_response = JSON.parse(self.response.body, symbolize_names: true)
 
-    assert_equal @user.email, json_response.dig(:data, :attributes, :email)
+    assert_equal @user.email, json_response[:data][:attributes][:email]
+    # assert_equal @user.email, json_response.dig(:data, :attributes, :email)
     # byebug
     # assert_equal @user.products.first.id.to_s, json_response.dig(:data, :relationships, :products, :data, 0, :id)
     # assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
