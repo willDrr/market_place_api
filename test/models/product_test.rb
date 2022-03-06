51e03b139bf29b34b00b044d34feeb463cba7c13
willdrr <willxdd97@gmail.com>
2022-03-06 00:56:26 +0000

    adds search scopes on the product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,7 +4,32 @@ class ProductTest < ActiveSupport::TestCase
   test 'should have a positive price' do
     product = products(:one)
     product.price = -1
-    #byebug
     assert_not product.valid?
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:another_tv), products(:one)],
+      Product.filter_by_title('tv').sort
+  end
+
+  test 'should filter products by price and sort them' do
+    assert_equal [products(:two), products(:one)],
+		  Product.above_or_equal_to_price(200).sort
+  end
+
+  test 'should filter products by price lower and sort them' do
+    assert_equal [products(:another_tv)], Product.below_or_equal_to_price(200).sort
+  end
+
+  test 'should sort product by most recent' do
+    products(:two).touch
+    assert_equal [products(:one), products(:another_tv), products(:two)],
+      Product.recent.to_a
+  end
+
+  test 'should sort product by most recent and title' do
+    products(:two).touch
+    assert_equal [products(:one), products(:another_tv), products(:two)].map(&:title),
+      Product.recent.map(&:title)
   end
 end
