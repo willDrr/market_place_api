898bbd070e5374174eb7ac29e049e4d5616d0798
willdrr <willxdd97@gmail.com>
2022-03-06 12:34:28 +0000

    adds the show action for order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults:{ format: :json } do
     namespace :v1 do
-      resources :orders, only: [:index]
+      resources :orders, only: %i[index show] 
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
       resources :products #, only: %i[show index create update]
