c5400ab390137a96e5039fcd0af7cc894fb9a1d7
willdrr <willxdd97@gmail.com>
2022-03-12 17:20:44 +0000

    adds caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -13,4 +13,6 @@ class OrderSerializer
 
   belongs_to :user
   has_many :products
+
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -15,4 +15,6 @@ class ProductSerializer
   include JSONAPI::Serializer
   attributes :title, :price, :published
   belongs_to :user
+
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
